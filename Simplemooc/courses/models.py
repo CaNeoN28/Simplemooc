4365be786559be2aeb08b7a449477a7346056d6e
CaNeoN28 <carlosfelipe.st28@gmail.com>
2021-12-08 14:34:45 -0300

    Model Manager em Course

--- Simplemooc/courses/models.py
+++ Simplemooc/courses/models.py
@@ -1,6 +1,14 @@
 from enum import auto
 from django.db import models
 
+class CourseManager(models.Manager):
+    def search(self, query):
+        #retorna o curso onde o nome ou a descrição contem a entrada de pesquisa
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query),
+            models.Q(description__icontains=query)
+        )
+
 class Course(models.Model):
     name = models.CharField("Nome", max_length=100)
     slug = models.SlugField('Atalho')
@@ -12,3 +20,5 @@ class Course(models.Model):
     updated_at = models.DateField('Editado em', auto_now=True)
 
     image = models.ImageField(upload_to = 'courses/images', verbose_name='Imagem', blank=True, null=True)
+
+    objects = CourseManager() #define o método de pesquisa da tabela
