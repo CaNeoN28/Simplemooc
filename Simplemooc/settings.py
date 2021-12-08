6db73ea9bddb925a8172872734c242c494f2252f
CaNeoN28 <carlosfelipe.st28@gmail.com>
2021-12-08 13:44:49 -0300

    criação da app cursos

--- Simplemooc/settings.py
+++ Simplemooc/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'Simplemooc.core'
+    'Simplemooc.core',
+    'Simplemooc.courses'
 ]
 
 MIDDLEWARE = [
