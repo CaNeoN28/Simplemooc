33802580b555dffa4ad0ce4d5dde3af1e6211505
CaNeoN28 <carlosfelipe.st28@gmail.com>
2021-12-08 14:54:59 -0300

    Personalização do admin

--- Simplemooc/courses/admin.py
+++ Simplemooc/courses/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Course
 
-admin.site.register(Course) #Possibilita a alteração dos cursos pelo site de administração
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created_at', 'start_date'] #Informações exibidas nas listas de cursos
+    search_fields = ['name', 'slug'] #Define os espaços onde pesquisar 
+
+admin.site.register(Course, CourseAdmin) #Possibilita a alteração dos cursos pelo site de administração
+# O "COURSEADMIN" chama a classe de personalização
