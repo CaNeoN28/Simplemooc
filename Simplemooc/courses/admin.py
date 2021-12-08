023d874dcd0a42dd25d3bdc9884ea96c59e2263c
CaNeoN28 <carlosfelipe.st28@gmail.com>
2021-12-08 15:01:01 -0300

    Adição da recomendação de slugs

--- Simplemooc/courses/admin.py
+++ Simplemooc/courses/admin.py
@@ -6,5 +6,8 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'created_at', 'start_date'] #Informações exibidas nas listas de cursos
     search_fields = ['name', 'slug'] #Define os espaços onde pesquisar 
 
+    prepopulated_fields = {'slug' : ('name',)} #Recomenda automaticamente um atalho com base no nome do curso
+                                               #OBS : A vírgula é estritamente necessária, caso contrário não será reconhecido uma lista
+
 admin.site.register(Course, CourseAdmin) #Possibilita a alteração dos cursos pelo site de administração
 # O "COURSEADMIN" chama a classe de personalização
