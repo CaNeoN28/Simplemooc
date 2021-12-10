c9ec3ca7d41a21549fad793edf0f3682d0df21ba
CaNeoN28 <nocosttolove@gmail.com>
2021-12-10 13:11:13 -0400

    alteração nos templates de cursos

--- Simplemooc/courses/views.py
+++ Simplemooc/courses/views.py
@@ -6,11 +6,11 @@ from .models import Course #Referencia a tabela Course do BD
 # Create your views here.
 
 def index(request):
-    course = Course.objects.all() #Possui todos os objetos da tabela Course 
+    courses = Course.objects.all() #Possui todos os objetos da tabela Course 
     template_name = 'courses/index.html'
 
     context = {
-        'course' : course #Adiciona course ao contexto do template
+        'courses' : courses #Adiciona course ao contexto do template
     }
     return render(request, template_name, context)
     
