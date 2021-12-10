0afa17613ef0c6479a3f2bb92abfc1c2c4c1a8fd
CaNeoN28 <nocosttolove@gmail.com>
2021-12-10 11:42:13 -0400

    Site inicial dos cursos

--- Simplemooc/courses/views.py
+++ Simplemooc/courses/views.py
@@ -1,9 +1,16 @@
+from typing import ContextManager
 from django.http import request
 from django.shortcuts import render
+from .models import Course #Referencia a tabela Course do BD
 
 # Create your views here.
 
 def index(request):
+    course = Course.objects.all() #Possui todos os objetos da tabela Course 
     template_name = 'courses/index.html'
-    return render(request, template_name)
+
+    context = {
+        'course' : course #Adiciona course ao contexto do template
+    }
+    return render(request, template_name, context)
     
