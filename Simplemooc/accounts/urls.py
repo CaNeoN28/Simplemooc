9693ff15b1c239cfaf5d332371a491ba7372f1a3
CaNeoN28 <nocosttolove@gmail.com>
2021-12-11 07:36:58 -0400

    Criação de área para cadastro

--- Simplemooc/accounts/urls.py
+++ Simplemooc/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.views import LoginView
+from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('entrar/', LoginView.as_view(template_name = 'accounts/login.html', next_page = None), name = 'login')
+    path('entrar/', LoginView.as_view(template_name = 'accounts/login.html', next_page = None), name = 'login'),
     #A classe LoginView deve ser passada com o método .asView()
     #O método deverá ter atributos para um template customizado
+    path('cadastro/', views.register, name='register')
 ]
--- Simplemooc/accounts/views.py
+++ Simplemooc/accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
 
-# Create your views here.
+def register(request):
+    template_name = 'accounts/register.html'
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid(): #Verifica se o form é valido
+            form.save() #Salva os dados em um objeto da tabela
+            return redirect(settings.LOGIN_URL) #Redireciona para a página de Login
+    else:
+        form = UserCreationForm()
+
+    context = {
+        'form' : form #Classe simples de criação de usuários do Django
+    }
+
+    return render(request, template_name, context)
