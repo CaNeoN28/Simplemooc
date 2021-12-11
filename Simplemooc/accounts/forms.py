496d5b6984ef187bad97627d4968677954bf805d
CaNeoN28 <nocosttolove@gmail.com>
2021-12-11 08:11:43 -0400

    Email agora é unico

--- Simplemooc/accounts/forms.py
+++ Simplemooc/accounts/forms.py
@@ -1,10 +1,18 @@
 from django.contrib.auth import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 
 class RegisterForm(UserCreationForm):
 
     email = forms.EmailField(label = 'E-Mail') #Adiciona email como field do form
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists(): # Verifica se o email ja está cadastrado
+            raise forms.ValidationError('Email já cadastrado!') # Retorna um erro se o email ja estiver cadastrado
+        
+        return email
 
     def save(self, commit=True): # Método para salvar o email do usuário separadamente(não padrão)
         user = super(RegisterForm, self).save(commit = False) # Não salva o usuário padrão
