e43dcbe3d878d38f95d1d95ee8e10bd3fc968f06
CaNeoN28 <nocosttolove@gmail.com>
2021-12-16 15:26:41 -0400

    Introdução a testes

--- Simplemooc/core/tests.py
+++ Simplemooc/core/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.urls import reverse
 
 # Create your tests here.
+
+class HomeViewTest(TestCase):
+
+    # As funções teste devem ter o nome de teste_alguma_coisa
+    
+    def test_home_status_code(self):
+        client = Client() # Inicializa o cliente
+        response = client.get(reverse('core:home')) # Recebe a url da view do core:home
+        self.assertEqual(response.status_code, 200) # Método de teste
+
+    def test_home_template_used(self):
+        client = Client()
+        response = client.get(reverse('core:home'))
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+        # Verifica se os templates home e base são usados na view core:home
