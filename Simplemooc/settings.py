c000a1f4a0354c43655c1033dbf081901e458f11
CaNeoN28 <nocosttolove@gmail.com>
2021-12-20 06:50:24 -0400

    Mudança de estilos

--- Simplemooc/settings.py
+++ Simplemooc/settings.py
@@ -155,10 +155,6 @@ ALLOWED_HOSTS = []
 STATIC_ROOT = os.path.join(BASE_DIR,'Simplemooc', 'staticfiles')
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
