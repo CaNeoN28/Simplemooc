abb988beb6fd41a44b0c1e5695fad80e17fd7b80
CaNeoN28 <nocosttolove@gmail.com>
2021-12-26 15:58:14 -0400

    Exibição e criação de respostas

--- Simplemooc/forum/views.py
+++ Simplemooc/forum/views.py
@@ -1,10 +1,12 @@
 from typing import Any
 from django.db.models import query
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView, View, ListView, DetailView
 from taggit.models import Tag
 
 from Simplemooc.forum.models import Thread
+from .forms import ReplyForm
 
 # Create your views here.
 
@@ -46,7 +48,28 @@ class ThreadView(DetailView):
     def get_context_data(self, **kwargs: Any):
         context =  super(ThreadView, self).get_context_data(**kwargs)
         context['tags'] = Thread.tags.all()
+        context['form'] = ReplyForm(self.request.POST or None)
         return context
+    
+    def post(self, request, *args:Any, **kwargs:Any):
+        if not request.user.is_authenticated:
+            messages.error(request, 'Você precisa estar logado para enviar respostas')
+            return redirect(self.request.path)
+
+        self.object = self.get_object()
+        context = self.get_context_data(object = self.object)
+        form = context['form']
+
+        if form.is_valid():
+            reply = form.save(commit=False) # Preenche os campos sem salvar, não há usuário definido, então
+            #é necessário
+            reply.author = self.request.user
+            reply.thread = self.object
+            reply.save()
+            messages.success(request, 'Resposta enviada com sucesso')
+            context['form'] = ReplyForm()
+
+        return render(request, 'forum/thread.html', context)
 
 index = ForumView.as_view()
 thread = ThreadView.as_view()
